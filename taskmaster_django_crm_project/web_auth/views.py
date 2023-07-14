282cfa2411611a90e7d4f261b0afb56089e08306
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-07-14 20:56:08 +0100

    create profile form

--- taskmaster_django_crm_project/web_auth/views.py
+++ taskmaster_django_crm_project/web_auth/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.http import HttpResponseRedirect
 
 from taskmaster_django_crm_project.web_auth.forms import RegisterUserForm
@@ -7,11 +8,13 @@ from django.contrib.auth import login as auth_login
 from django.views import generic as views
 from django.urls import reverse_lazy
 
+from taskmaster_django_crm_project.web_auth.models import Profile
+
 
 class RegisterUserView(views.CreateView):
     template_name = 'web_auth/register.html'
     form_class = RegisterUserForm
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('create_profile')
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -28,8 +31,23 @@ class LogoutUserView(auth_views.LogoutView):
     template_name = 'web_auth/logout.html'
 
 
+class CreateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['is_deleted', 'user']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'})
+        }
+
+
 class CreateProfileView(views.CreateView):
-    pass
+    template_name = 'web_auth/create_profile.html'
+    form_class = CreateProfileForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class UpdateProfileView(views.CreateView):
