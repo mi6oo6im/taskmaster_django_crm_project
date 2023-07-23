e5f4c0b9f0becc9f681c13a7789ea8ff7e34aaa4
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-07-23 18:47:20 +0100

    update on tasks

--- taskmaster_django_crm_project/taskmaster/urls.py
+++ taskmaster_django_crm_project/taskmaster/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import DisplayHomepageView, DisplayDashboardView, CreateCustomerView, UpdateCustomerView, DeleteCustomerView, CreateTaskView,\
-    UpdateTaskView, DeleteTaskView, DisplayAllTasksView, DisplayAllCustomersView
+from .views import DisplayHomepageView, DisplayDashboardView, CreateCustomerView, UpdateCustomerView, \
+    DeleteCustomerView, CreateTaskView, \
+    UpdateTaskView, DeleteTaskView, DisplayAllTasksView, DisplayAllCustomersView, CompleteTaskView, PendingTaskView
 
 urlpatterns = (
     path('', DisplayHomepageView.as_view(), name='index'),
@@ -11,6 +12,8 @@ urlpatterns = (
     path('customer/delete/<int:pk>/', DeleteCustomerView.as_view(), name='delete_customer'),
     path('task/create/', CreateTaskView.as_view(), name='create_task'),
     path('task/all/', DisplayAllTasksView.as_view(), name='my_tasks'),
+    path('task/complete/<int:pk>/', CompleteTaskView.as_view(), name='complete_task'),
+    path('task/pending/<int:pk>/', PendingTaskView.as_view(), name='pending_task'),
     path('task/update/<int:pk>/', UpdateTaskView.as_view(), name='update_task'),
     path('task/delete/<int:pk>/', DeleteTaskView.as_view(), name='delete_task'),
 )
--- taskmaster_django_crm_project/taskmaster/views.py
+++ taskmaster_django_crm_project/taskmaster/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 # Create your views here.
 # Get the user module for auth view:
 from django.views.generic import CreateView, TemplateView, UpdateView, ListView
 
+from taskmaster_django_crm_project.taskmaster.forms import UpdateTaskForm
 from taskmaster_django_crm_project.taskmaster.models import Task, Customer
 
 UserModel = get_user_model()
@@ -57,11 +59,42 @@ class DisplayAllTasksView(ListView):
 
         queryset = Task.objects.filter(company__in=customers, is_deleted=False)
 
+        queryset = queryset.order_by('completed', 'due_date')
+
         return queryset
 
 
-class UpdateTaskView(CreateView):
-    pass
+class CompleteTaskView(UpdateView):
+    model = Task
+    fields = []
+    template_name = 'taskmaster/complete_task.html'
+    success_url = reverse_lazy('my_tasks')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        form.instance.completed = True
+        self.object.save()
+        return super().form_valid(form)
+
+
+class PendingTaskView(UpdateView):
+    model = Task
+    fields = []
+    template_name = 'taskmaster/pending_task.html'
+    success_url = reverse_lazy('my_tasks')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        form.instance.completed = False
+        self.object.save()
+        return super().form_valid(form)
+
+
+class UpdateTaskView(UpdateView):
+    model = Task
+    template_name = 'taskmaster/update_task.html'
+    form_class = UpdateTaskForm
+    success_url = reverse_lazy('my_tasks')
 
 
 class DeleteTaskView(CreateView):
